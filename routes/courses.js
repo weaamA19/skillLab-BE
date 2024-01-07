f915669f5e04adcf79382828ec154060f6930c0b
04maryam <itsmaryamali54@gmail.com>
2024-01-07 17:03:36 +0300

    updated

--- routes/courses.js
+++ routes/courses.js
@@ -12,13 +12,13 @@ const coursesCtrl = require('../controllers/courses');
 const {checkType} = require('../config/checkType');
 
 //Routs
-router.get("/add", isLoggedIn, checkType(1), coursesCtrl.courses_add_get);
-router.post("/add",isLoggedIn, checkType(1), coursesCtrl.courses_add_post);
+router.get("/add", isLoggedIn, coursesCtrl.courses_add_get);
+router.post("/add",isLoggedIn, coursesCtrl.courses_add_post);
 router.get("/index", coursesCtrl.courses_index_get);
 router.get("/detail/:id", coursesCtrl.courses_show_get);
-router.delete("/delete", isLoggedIn, checkType(1), coursesCtrl.courses_delete_get);
-router.get("/edit", isLoggedIn, checkType(1), coursesCtrl.courses_edit_get);
-router.put("/update", isLoggedIn, checkType(1), coursesCtrl.courses_update_post);
+router.delete("/delete", isLoggedIn, coursesCtrl.courses_delete_get);
+router.get("/edit", isLoggedIn, coursesCtrl.courses_edit_get);
+router.put("/update", isLoggedIn, coursesCtrl.courses_update_post);
 router.get('/coursesByCategory/:id', coursesCtrl.CoursesByCategory_get);
 
 
